76fa4f607cb6ae9e943652695f1dce16dbc14d9d
Eric Berglund <ericberglund117@gmail.com>
2020-11-10 10:48:04 -0700

    Pass second it block in form test

--- src/components/UrlForm/UrlForm.test.js
+++ src/components/UrlForm/UrlForm.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import UrlForm from './UrlForm'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
 
 describe('UrlForm', () => {
   it('When the Form is rendered, make sure that the correct elements render on the dom', () => {
@@ -17,7 +18,16 @@ describe('UrlForm', () => {
   });
 
   it('When the inputs change, make sure that the form elements hold the correct values', () => {
+    render(<UrlForm />)
 
+    const titleInput = screen.getByPlaceholderText('Title...');
+    const urlInput = screen.getByPlaceholderText('URL to Shorten...');
+
+    userEvent.type(titleInput, 'Hot Rod')
+    expect(titleInput).toHaveValue('Hot Rod')
+
+    userEvent.type(urlInput, 'www.longUrl.com')
+    expect(urlInput).toHaveValue('www.longUrl.com')
   });
 
   it('When the form is submitted, make sure any appropriate functions are called.', () => {
