1d398b37746121bc7f10d0510d21ec6e589d1ec3
Eric Berglund <ericberglund117@gmail.com>
2020-11-10 13:48:34 -0700

    Update final it block to pass all of App test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -20,14 +20,15 @@ describe('App', () => {
       title: "Hot Rod"
     }]};
 
-    const postedUrl = {
+    const postedUrl = { urls: [{
       id: 3,
-      long_url:"http://www.frenchtoastsunday.com/2012/08/top-10-quotes-from-hot-rod.html",
-      short_url: "http://localhost:3001/useshorturl/2",
+      long_url:"http:looooooooooooong.html",
+      short_url: "http://short.html",
       title: "Bangers and Mash"
-    }
+    }]}
 
   it('When the App is rendered, make sure any UI specific to the App component renders as well', () => {
+    getUrls.mockResolvedValueOnce(expectedUrls)
     render(<App />)
 
     const header = screen.getByText('URL Shortener');
@@ -38,7 +39,6 @@ describe('App', () => {
     getUrls.mockResolvedValueOnce(expectedUrls)
     render(<App />)
 
-    await waitFor(() => expect(getUrls).toHaveBeenCalledTimes(1))
     const urlOneTitle = await waitFor(() => screen.getByText("Awesome photo"));
     const urlOneLong = await waitFor(() => screen.getByText("https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"));
     const urlOneShort = await waitFor(() => screen.getByText("http://localhost:3001/useshorturl/1"));
@@ -49,10 +49,12 @@ describe('App', () => {
 
   it('When the App renders, make sure that users can fill out the form, submit the form, and see a new url added to the DOM', async () => {
     postUrls.mockResolvedValueOnce(postedUrl);
+    getUrls.mockResolvedValueOnce(expectedUrls);
     render(<App />)
 
     const urlOneTitle = await waitFor(() => screen.getByText("Awesome photo"));
     expect(urlOneTitle).toBeInTheDocument();
+
     const titleInput = screen.getByPlaceholderText('Title...');
     const urlInput = screen.getByPlaceholderText('URL to Shorten...');
     const button = screen.getByRole('button', { name: /shorten please!/i })
@@ -64,12 +66,15 @@ describe('App', () => {
     userEvent.type(titleInput, 'Bangers and Mash')
     expect(titleInput).toHaveValue('Bangers and Mash')
 
-    userEvent.type(urlInput, 'http://www.frenchtoastsunday.com/2012/08/top-10-quotes-from-hot-rod.html')
-    expect(urlInput).toHaveValue('http://www.frenchtoastsunday.com/2012/08/top-10-quotes-from-hot-rod.html')
+    userEvent.type(urlInput, "http:looooooooooooong.html")
+    expect(urlInput).toHaveValue("http:looooooooooooong.html")
     userEvent.click(button)
 
     await waitFor(() => expect(postUrls).toHaveBeenCalledTimes(1))
     const newTitle = await waitFor(() => screen.getByText('Bangers and Mash'))
+    const newShortUrl = await waitFor(() => screen.getByText("http://short.html"))
     expect(newTitle).toBeInTheDocument();
+    expect(urlInput).toBeInTheDocument();
+    expect(newShortUrl).toBeInTheDocument();
   })
 });
