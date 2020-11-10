f1b065352b2b538fa757bb9862f2d6858ee66ab1
Eric Berglund <ericberglund117@gmail.com>
2020-11-10 12:06:00 -0700

    Bug not passing last it block in App test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import App from './App'
-import UrlContainer from '../UrlContainer/UrlContainer'
-import { render, waitFor, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import userEvent from '@testing-library/user-event'
-import { getUrls, postUrls } from '../../apiCalls'
+import UrlContainer from '../UrlContainer/UrlContainer';
+import UrlForm from '../UrlForm/UrlForm'
+import { render, waitFor, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import { getUrls, postUrls } from '../../apiCalls';
 jest.mock('../../apiCalls');
 
 const expectedUrls = [{
@@ -19,6 +20,13 @@ const expectedUrls = [{
   short_url: "http://localhost:3001/useshorturl/2",
   title: "Hot Rod"
 }];
+
+const postedUrl = [{
+  id: 3,
+  long_url:"http://www.frenchtoastsunday.com/2012/08/top-10-quotes-from-hot-rod.html",
+  short_url: "http://localhost:3001/useshorturl/2",
+  title: "Hot Rod"
+}]
 
 describe('App', () => {
   it('When the App is rendered, make sure any UI specific to the App component renders as well', () => {
@@ -37,7 +45,7 @@ describe('App', () => {
     render(<App />)
 
     render(<UrlContainer urls={expectedUrls}/>)
-    
+    screen.debug()
     const urlOneTitle = screen.getByText("Awesome photo")
     const urlOneLong = screen.getByText("https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80")
     const urlOneShort = screen.getByText("http://localhost:3001/useshorturl/1")
@@ -46,8 +54,32 @@ describe('App', () => {
     expect(urlOneLong).toBeInTheDocument();
     expect(urlOneShort).toBeInTheDocument();
   });
-  //
-  // it('When the App renders, make sure that users can fill out the form, submit the form, and see a new url added to the DOM', () => {
-  //   render(<App />)
-  // })
+
+  it('When the App renders, make sure that users can fill out the form, submit the form, and see a new url added to the DOM', () => {
+    getUrls.mockResolvedValue(expectedUrls)
+    postUrls.mockResolvedValue(postedUrl)
+    render(<App />)
+
+    const titleInput = screen.getByPlaceholderText('Title...');
+    const urlInput = screen.getByPlaceholderText('URL to Shorten...');
+    const button = screen.getByRole('button', { name: /shorten please!/i })
+
+    expect(titleInput).toBeInTheDocument();
+    expect(urlInput).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+
+    userEvent.type(titleInput, 'Hot Rod')
+    expect(titleInput).toHaveValue('Hot Rod')
+
+    userEvent.type(urlInput, 'www.longUrl.com')
+    expect(urlInput).toHaveValue('www.longUrl.com')
+
+    const fetch = jest.fn()
+    
+    render(<UrlForm fetch={fetch}/>)
+
+    userEvent.click(button)
+
+    expect(fetch).toHaveBeenCalled()
+  })
 });
