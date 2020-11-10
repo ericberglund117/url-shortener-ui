69529dd078882cff0b14fe7893345e235d4aa0e8
Eric Berglund <ericberglund117@gmail.com>
2020-11-10 13:43:39 -0700

    Update expectedUrls to pass the second it block in App Test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -7,8 +7,7 @@ import { getUrls, postUrls } from '../../apiCalls';
 jest.mock('../../apiCalls');
 
 describe('App', () => {
-  beforeEach(() => {
-    const expectedUrls = [{
+    const expectedUrls = {urls: [{
       id: 1,
       long_url: "https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
       short_url: "http://localhost:3001/useshorturl/1",
@@ -19,18 +18,14 @@ describe('App', () => {
       long_url:"http://www.frenchtoastsunday.com/2012/08/top-10-quotes-from-hot-rod.html",
       short_url: "http://localhost:3001/useshorturl/2",
       title: "Hot Rod"
-    }];
+    }]};
 
-    const postedUrl = [{
+    const postedUrl = {
       id: 3,
       long_url:"http://www.frenchtoastsunday.com/2012/08/top-10-quotes-from-hot-rod.html",
       short_url: "http://localhost:3001/useshorturl/2",
       title: "Bangers and Mash"
-    }]
-
-    getUrls.mockResolvedValue(expectedUrls)
-    postUrls.mockResolvedValue(postedUrl);
-  });
+    }
 
   it('When the App is rendered, make sure any UI specific to the App component renders as well', () => {
     render(<App />)
@@ -40,18 +35,24 @@ describe('App', () => {
   });
 
   it('When the App renders, make sure that any urls on the server are added to the dom', async () => {
+    getUrls.mockResolvedValueOnce(expectedUrls)
     render(<App />)
 
     await waitFor(() => expect(getUrls).toHaveBeenCalledTimes(1))
     const urlOneTitle = await waitFor(() => screen.getByText("Awesome photo"));
+    const urlOneLong = await waitFor(() => screen.getByText("https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"));
+    const urlOneShort = await waitFor(() => screen.getByText("http://localhost:3001/useshorturl/1"));
     expect(urlOneTitle).toBeInTheDocument();
     expect(urlOneLong).toBeInTheDocument();
     expect(urlOneShort).toBeInTheDocument();
   });
 
   it('When the App renders, make sure that users can fill out the form, submit the form, and see a new url added to the DOM', async () => {
+    postUrls.mockResolvedValueOnce(postedUrl);
     render(<App />)
 
+    const urlOneTitle = await waitFor(() => screen.getByText("Awesome photo"));
+    expect(urlOneTitle).toBeInTheDocument();
     const titleInput = screen.getByPlaceholderText('Title...');
     const urlInput = screen.getByPlaceholderText('URL to Shorten...');
     const button = screen.getByRole('button', { name: /shorten please!/i })
@@ -66,6 +67,7 @@ describe('App', () => {
     userEvent.type(urlInput, 'http://www.frenchtoastsunday.com/2012/08/top-10-quotes-from-hot-rod.html')
     expect(urlInput).toHaveValue('http://www.frenchtoastsunday.com/2012/08/top-10-quotes-from-hot-rod.html')
     userEvent.click(button)
+
     await waitFor(() => expect(postUrls).toHaveBeenCalledTimes(1))
     const newTitle = await waitFor(() => screen.getByText('Bangers and Mash'))
     expect(newTitle).toBeInTheDocument();
